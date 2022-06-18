5bc2061ede812479713f2db274b63f423f3abd45
Юра Войтюк <voutuk@icloud.com>
2022-06-18 20:14:22 +0300

    Тест деплой
    Тест пайтон бота на вебхуках

---
+++ main.py
@@ -0,0 +1,48 @@
+import logging
+import os
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram import Bot, types
+
+
+TOKEN = os.getenv('5349715079:AAGxtfeDKtTXyf6y3T4DCQNRjQbfg0W_70Q')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+HEROKU_APP_NAME = os.getenv('pi-py-pi')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{pi-py-pi}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{5349715079:AAGxtfeDKtTXyf6y3T4DCQNRjQbfg0W_70Q}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+ if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
