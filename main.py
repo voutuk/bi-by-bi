6eaf1feff5c20e1978a0ed3df4cda4e8c9f22684
Юра Войтюк <voutuk@icloud.com>
2022-06-18 20:40:46 +0300

    Test deploy
    Fix eror

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
 
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = os.getenv('5349715079:AAGxtfeDKtTXyf6y3T4DCQNRjQbfg0W_70Q')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+HEROKU_APP_NAME = os.getenv('pi-py-pi')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
@@ -35,14 +35,14 @@ async def echo(message: types.Message):
     await message.answer(message.text)
 
 
- if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    if __name__ == '__main__':
+        logging.basicConfig(level=logging.INFO)
+        start_webhook(
+            dispatcher=dp,
+            webhook_path=WEBHOOK_PATH,
+            skip_updates=True,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+        )
